ad20979114da689ad6123f03d7e8215967bee54b
Thiago Santos <tsantos7098@gmail.com>
2023-07-24 11:55:03 -0300

    Desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,19 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
+        public int Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private int IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(int numero, string modelo, int imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia(numero: 12345, modelo: "Lumia", imei:  655464, memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+Console.WriteLine("");
+Smartphone iphone = new Iphone(numero: 546565, modelo: "Iphone12", imei: 6616226, memoria: 126);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
